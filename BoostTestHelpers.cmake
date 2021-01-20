a18394b9f24aae1b553fbf2632966224895e629c
Tristan Martindale <trisan.martindale@se-toolkit.app>
2021-01-20 21:38:49 +0100

    [build] tweak cmake

--- BoostTestHelpers.cmake
+++ BoostTestHelpers.cmake
@@ -1,3 +1,5 @@
+# Refer to: https://eb2.co/blog/2015/06/driving-boost.test-with-cmake/
+
 function(add_boost_test SOURCE_FILE_NAME)
 	get_filename_component(TEST_EXECUTABLE_NAME ${SOURCE_FILE_NAME} NAME_WE)
 
@@ -15,15 +17,6 @@ endfunction()
 
 function(add_boost_test_linked SOURCE_FILE_NAME DEPENDENCY_LIB)
 	get_filename_component(TEST_EXECUTABLE_NAME ${SOURCE_FILE_NAME} NAME_WE)
-
-	add_executable(${TEST_EXECUTABLE_NAME} ${SOURCE_FILE_NAME})
+	add_boost_test(SOURCE_FILE_NAME)
 	target_link_libraries(${TEST_EXECUTABLE_NAME} ${DEPENDENCY_LIB} ${Boost_UNIT_TEST_FRAMEWORK_LIBRARY})
-
-	file(READ "${SOURCE_FILE_NAME}" SOURCE_FILE_CONTENTS)
-	string(REGEX MATCHALL "BOOST_AUTO_TEST_CASE\\( *([A-Za-z_0-9]+) *\\)" FOUND_TESTS ${SOURCE_FILE_CONTENTS})
-
-	foreach(HIT ${FOUND_TESTS})
-			string(REGEX REPLACE ".*\\( *([A-Za-z_0-9]+) *\\).*" "\\1" TEST_NAME ${HIT})
-			add_test(NAME "${TEST_EXECUTABLE_NAME}.${TEST_NAME}" COMMAND ${TEST_EXECUTABLE_NAME} --run_test=${TEST_NAME} --catch_system_error=yes)
-	endforeach()
 endfunction()
