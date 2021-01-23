76a2cdc8551dd75e0dea9d886b68f296bf4d9d4e
Tristan Martindale <trisan.martindale@se-toolkit.app>
2021-01-23 17:45:34 +0100

    [tweak] add timing

--- src/main.cpp
+++ src/main.cpp
@@ -3,25 +3,45 @@
 #include <iostream>
 #include <string_view>
 
+#include <boost/timer/timer.hpp>
+
 #include "LinearMotionModel.hpp"
 
+using namespace boost::timer;
 using namespace LinearMotion;
 
 int main()
 {
+   cpu_timer timer;
+
    LinearMotionModel model{};
+   model.setInput(1.0);
    const State &state = model.getOutput();
 
    auto printState{
-       [&state]() {
-          std::cout << "Model output: position = " << state.position << ", velocity = " << state.velocity << ", acceleration = " << state.acceleration << '\n';
+       [&timer, &state]() {
+          std::cout
+              << "Elapsed time = " << timer.elapsed().wall * 1e-6
+              << "; Model output: position = " << state.position
+              << ", velocity = " << state.velocity
+              << ", acceleration = " << state.acceleration
+              << '\n';
        }};
 
-   printState();
-   model.setInput(1.0);
-   printState();
-   model.run();
-   printState();
+   auto runInSchedule{
+       [&timer, &model, &printState](LinearMotionModel &model) {
+          int i = 0;
+          while (i < 10)
+          {
+             timer.start();
+             model.run();
+             timer.stop();
+             printState();
+             i++;
+          }
+       }};
+
+   runInSchedule(model);
 
    return 0;
 }
